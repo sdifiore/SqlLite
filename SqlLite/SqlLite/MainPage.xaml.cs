ae4391bbecb4654747693b77217c829c29a2c40d
Sergio Di Fiore <difiores@outlook.com>
2018-06-06 10:30:15 -0300

    crud

--- SqlLite/SqlLite/MainPage.xaml.cs
+++ SqlLite/SqlLite/MainPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using HelloWorld;
+using SQLite;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +11,70 @@ using Xamarin.Forms;
 
 namespace SqlLite
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public class Recipe : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        private string _name;
+
+        [MaxLength(255)]
+        public string Name 
+       {
+            get { return _name; }
+            set
+            {
+                if (_name == value) return;
+
+                _name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        private SQLiteAsyncConnection _connection;
+        private ObservableCollection<Recipe> _recipes;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _connection = DependencyService.Get<ISQLiteDb>().GetConnection();
+        }
+
+        protected override async void OnAppearing()
+        {
+            await _connection.CreateTableAsync<Recipe>();
+            var recipes = await _connection.Table<Recipe>().ToListAsync();
+            _recipes = new ObservableCollection<Recipe>(recipes);
+            recipesListView.ItemsSource = _recipes;
+
+            base.OnAppearing();
+        }
+
+        private async void OnAdd(object sender, System.EventArgs e)
+        {
+            var recipe = new Recipe { Name = $"Recipe {DateTime.Now.Ticks}" };
+            await _connection.InsertAsync(recipe);
+            _recipes.Add(recipe);
+        }
+
+        private async void OnUpdate(object sender, System.EventArgs e)
+        {
+            var recipe = _recipes[0];
+            recipe.Name += "UPDATED";
+            await _connection.UpdateAsync(recipe);
+        }
+
+        private async void OnDelete(object sender, System.EventArgs e)
+        {
+            var recipe = _recipes[0];
+            await _connection.DeleteAsync(recipe);
+            _recipes.Remove(recipe);
+        }
+    }
 }
